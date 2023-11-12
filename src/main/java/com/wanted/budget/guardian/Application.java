d51d11172aecc4619cb1f171836a60b592922e5b
min050410 <min050410@gmail.com>
2023-11-12 23:51:11 +0900

    init: Spring/Jpa/Java 프로젝트 생성

---
+++ src/main/java/com/wanted/budget/guardian/Application.java
@@ -0,0 +1,13 @@
+package com.wanted.budget.guardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
