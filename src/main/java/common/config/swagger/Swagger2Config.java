8710c6ee8d739d933bf7dd69c48625fe18e6b1ef
min050410 <min050410@gmail.com>
2023-11-14 13:51:02 +0900

    feat(Config): 프로젝트 기본 디펜던시들을 설정한다. - querydsl 설정 - spring security, jwt 설정 - swagger 설정 - p6spy 설정

---
+++ src/main/java/common/config/swagger/Swagger2Config.java
@@ -0,0 +1,36 @@
+package common.config.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Swagger2Config {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+            .version("v1.0.0")
+            .title("Budget Guardian API")
+            .description("예산 가디언의 API 명세서 입니다.");
+
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+            .name(jwt)
+            .type(SecurityScheme.Type.APIKEY)
+            .in(SecurityScheme.In.HEADER)
+            .name("token")
+        );
+
+        return new OpenAPI()
+            .info(info)
+            .addSecurityItem(securityRequirement)
+            .components(components);
+    }
+
+}
