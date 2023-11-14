cce78eff0e7db5be7097933f92dfa5b6bb76a0d1
min050410 <min050410@gmail.com>
2023-11-15 00:07:48 +0900

    feat(Auth): ApiPath를 작성한다.

---
+++ src/main/java/com/wanted/budget/guardian/app/web/path/ApiPath.java
@@ -0,0 +1,15 @@
+package com.wanted.budget.guardian.app.web.path;
+
+public class ApiPath {
+
+    // 에러 핸들러
+    public static final String ERROR_AUTH = "/api/v1/error";
+
+    // 인증
+    public static final String SIGNUP = "/api/v1/signup";
+    public static final String LOGIN = "/api/v1/login";
+    public static final String REFRESH_TOKEN = "/api/v1/oauth/refresh";
+    public static final String VALIDATE_TOKEN = "/api/v1/oauth/validate";
+    public static final String LOGOUT = "/api/v1/logout";
+
+}
