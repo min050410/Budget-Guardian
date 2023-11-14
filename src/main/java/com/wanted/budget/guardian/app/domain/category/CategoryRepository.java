7705bad1bcf1c92c4d6fceba81950f2267bc00ec
min050410 <min050410@gmail.com>
2023-11-15 00:40:34 +0900

    feat(Category): 카테고리 목록 조회 서비스 구현

---
+++ src/main/java/com/wanted/budget/guardian/app/domain/category/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.wanted.budget.guardian.app.domain.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
