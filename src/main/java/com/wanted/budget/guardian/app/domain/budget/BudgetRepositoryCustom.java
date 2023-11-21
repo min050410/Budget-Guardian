b5e2b466ab806c8c93040289f70561bb2ff80019
min050410 <min050410@gmail.com>
2023-11-21 23:55:10 +0900

    feat(Budget): Budget의PerOfBudget 필드의 평균을 각 카테고리 별로 계산한다.

--- src/main/java/com/wanted/budget/guardian/app/domain/budget/BudgetRepositoryCustom.java
+++ src/main/java/com/wanted/budget/guardian/app/domain/budget/BudgetRepositoryCustom.java
@@ -2,9 +2,12 @@ package com.wanted.budget.guardian.app.domain.budget;
 
 import com.wanted.budget.guardian.app.domain.category.Category;
 import com.wanted.budget.guardian.app.domain.member.Member;
+import java.util.Map;
 
 public interface BudgetRepositoryCustom {
 
     Budget findDuplicateBudget(Member member, Category category);
+
+    Map<Category, Double> getAveragePerOfBudgetPerCategory();
 
 }
