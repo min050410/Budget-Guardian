eeedfc76b5df2565ba028be156b596c6ca15c517
min050410 <min050410@gmail.com>
2023-11-21 15:55:44 +0900

    feat(Budget): 예산 설정 요청을 DTO list로 수정한다.

--- src/main/java/com/wanted/budget/guardian/app/web/controller/budget/BudgetController.java
+++ src/main/java/com/wanted/budget/guardian/app/web/controller/budget/BudgetController.java
@@ -1,17 +1,34 @@
 package com.wanted.budget.guardian.app.web.controller.budget;
 
-import com.wanted.budget.guardian.app.web.dto.budget.BudgetIdResponseDto;
+import com.wanted.budget.guardian.app.domain.budget.BudgetService;
+import com.wanted.budget.guardian.app.web.dto.budget.CreateBudgetRequestDto;
 import com.wanted.budget.guardian.app.web.path.ApiPath;
+import com.wanted.budget.guardian.common.config.security.context.LoginMember;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "예산")
+@RequiredArgsConstructor
+@RestController
 public class BudgetController {
 
+    private final BudgetService budgetService;
+
     @Operation(summary = "예산 설정")
-    @GetMapping(ApiPath.BUDGET)
-    public ResponseEntity<BudgetIdResponseDto> createBudget() {
-//        return ResponseEntity.ok();
+    @PostMapping(ApiPath.BUDGET)
+    public ResponseEntity<Void> configureBudget(
+        @AuthenticationPrincipal LoginMember loginMember,
+        @Valid @RequestBody CreateBudgetRequestDto body
+    ) {
+        budgetService.configureBudget(loginMember, body);
+        return ResponseEntity.ok().build();
     }
 
 }
