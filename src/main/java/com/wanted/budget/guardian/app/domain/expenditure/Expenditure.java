1d672e2442716b12c121a6d8af6d4ce8f10e166a
min050410 <min050410@gmail.com>
2023-11-23 23:35:21 +0900

    fix: 지출 Entity 필드 isIncludingExpenditure -> allowsSumCalculation으로 변경

--- src/main/java/com/wanted/budget/guardian/app/domain/expenditure/Expenditure.java
+++ src/main/java/com/wanted/budget/guardian/app/domain/expenditure/Expenditure.java
@@ -37,7 +37,7 @@ public class Expenditure extends BaseCreateTimeEntity {
 
     @Column(nullable = false)
     @ColumnDefault("true")
-    private boolean isIncludingExpenditure = true;
+    private boolean allowsSumCalculation = true;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
