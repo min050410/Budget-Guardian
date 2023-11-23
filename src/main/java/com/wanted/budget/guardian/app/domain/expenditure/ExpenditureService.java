9ed974922f839c5d5b49ab6618d0b65c26906c13
min050410 <min050410@gmail.com>
2023-11-23 10:21:55 +0900

    feat(Expenditure): 지출 상세 조회 로직을 작성한다.

--- src/main/java/com/wanted/budget/guardian/app/domain/expenditure/ExpenditureService.java
+++ src/main/java/com/wanted/budget/guardian/app/domain/expenditure/ExpenditureService.java
@@ -6,9 +6,13 @@ import com.wanted.budget.guardian.app.domain.member.Member;
 import com.wanted.budget.guardian.app.domain.member.MemberService;
 import com.wanted.budget.guardian.app.web.dto.expenditure.CreateExpenditureRequestDto;
 import com.wanted.budget.guardian.app.web.dto.expenditure.ExpenditureIdResponseDto;
+import com.wanted.budget.guardian.app.web.dto.expenditure.ExpenditureDetailResponseDto;
 import com.wanted.budget.guardian.common.config.security.context.LoginMember;
+import com.wanted.budget.guardian.common.exception.ExpenditureNotFoundException;
+import com.wanted.budget.guardian.common.exception.NotPossibleToAccessExpenditureException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -33,5 +37,25 @@ public class ExpenditureService {
         return ExpenditureIdResponseDto.of(expenditure.getId());
     }
 
+
+    /**
+     * 지출 상세 조회
+     */
+    public ExpenditureDetailResponseDto findExpenditureDetail(LoginMember loginMember, Long expenditureId) {
+        Member member = memberService.findLoginMember(loginMember);
+        Expenditure expenditure = findById(expenditureId);
+
+        if (!expenditure.isAccessibleToExpenditure(member)) {
+            throw new NotPossibleToAccessExpenditureException();
+        }
+
+        return ExpenditureDetailResponseDto.of(expenditure);
+    }
+
+    @Transactional(readOnly = true)
+    public Expenditure findById(Long expenditureId) {
+        return expenditureRepository.findById(expenditureId)
+            .orElseThrow(ExpenditureNotFoundException::new);
+    }
 
 }
